57a391897e68077b60cd2246b07d82f991a3f089
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-09-20 11:45:39 -0300

    asad

---
+++ src/app/pages/add-player/add-player.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../../model/player';
+
+@Component({
+  selector: 'app-add-player',
+  templateUrl: './add-player.page.html',
+  styleUrls: ['./add-player.page.scss'],
+})
+export class AddPlayerPage implements OnInit {
+  protected player: Player = new Player;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAz9K_YsG-YEv1-ZuKGxYKDkPNtG99LhNY",
+    authDomain: "sharegames-60def.firebaseapp.com",
+    databaseURL: "https://sharegames-60def.firebaseio.com",
+    projectId: "sharegames-60def",
+    storageBucket: "",
+    messagingSenderId: "252531551158",
+    appId: "1:252531551158:web:c0e03bed9460ae11802f17"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
